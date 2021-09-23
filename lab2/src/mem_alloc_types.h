0aa342a57ebc657cf39bef8993d5ef035f5e8333
Ioana Baciu <ioana.baciu@etu.univ-grenoble-alpes.fr>
2021-09-23 14:44:19 +0000

    first lab progress

---
+++ lab2/src/mem_alloc_types.h
@@ -0,0 +1,24 @@
+#ifndef   	_MEM_ALLOC_TYPES_H_
+#define   	_MEM_ALLOC_TYPES_H_
+
+
+
+/* Structure declaration for a free block */
+struct mem_free_block{
+    // int magic;
+    size_t size;
+    /* TODO: DEFINE */
+    struct mem_free_block *next;
+}; 
+typedef struct mem_free_block mem_free_block_t; 
+
+/* Specific metadata for used blocks */
+struct mem_used_block{
+    // int magic;
+    size_t size;
+    /* TODO: DEFINE */
+};
+typedef struct mem_used_block mem_used_block_t;
+
+
+#endif
